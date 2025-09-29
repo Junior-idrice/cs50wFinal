0514dc8fadc8bf7bb6f712438ff7d1e27c14edca
idrice junior <dongmojunior18@gmail.com>
2025-09-29 15:34:23 -0500

    models

--- yt_notes/noteapp/models.py
+++ yt_notes/noteapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Note(models.Model):
+    CATEGORY = (('BUSINESS','Business'),
+                ('PERSONLA','Personal'),
+                ('IMPORTANT','Important'))
+    
+
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    slug = models.SlugField(unique=True)
+    category = models.CharField(max_length=15, choices=CATEGORY, default="PERSONAL")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
