5190042fdae1ba4554dabc2f42d5eae96e7527bf
idrice junior <dongmojunior18@gmail.com>
2025-09-30 21:14:32 -0500

    done backend

--- yt_notes/noteapp/urls.py
+++ yt_notes/noteapp/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path("notes", views.notes, name = "notes")
+    path("notes/", views.notes, name = "notes"),
+    path("notes/<slug:slug>", views.note_details, name="note-details")
 ]
+
+#end point:
+#http://127.0.0.1:8000/notes/
+#http://127.0.0.1:8000/notes/note-slug/
--- yt_notes/yt_notes/settings.py
+++ yt_notes/yt_notes/settings.py
@@ -31,7 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'noteapp',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
