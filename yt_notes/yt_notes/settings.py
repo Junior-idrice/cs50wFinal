53c7f679c46efb5e4a83dfdf9423601112f7d9aa
idrice junior <dongmojunior18@gmail.com>
2025-10-04 14:01:03 -0500

    added

--- yt_notes/yt_notes/settings.py
+++ yt_notes/yt_notes/settings.py
@@ -29,9 +29,13 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+#ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
-
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "cs50wfinal-1.onrender.com",
+]
 
 # Application definition
 
