be9dde4984332c766cc41740d4b5459fc5d047dc
idrice junior <96833650+Junior-idrice@users.noreply.github.com>
2025-10-05 21:51:21 -0500

    Update views.py

--- yt_notes/noteapp/views.py
+++ yt_notes/noteapp/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 from django.db.models import Q
-# Create your views here.
+
 
 @api_view(["GET","POST"])
 def notes(request):
