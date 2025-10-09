ba828a46c86602c1e81ec092f97ad04dce74d2a2
idrice junior <dongmojunior18@gmail.com>
2025-10-08 22:45:05 -0500

    all

--- yt_notes/noteapp/models.py
+++ yt_notes/noteapp/models.py
@@ -21,7 +21,7 @@ class Note(models.Model):
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.title
+        return f"{self.title} - {self.user.username}"
 
     
     '''def save(self, *args, **kwargs):
--- yt_notes/noteapp/serializers.py
+++ yt_notes/noteapp/serializers.py
@@ -4,4 +4,4 @@ from .models import Note
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
-        fields = ["id","title","body","slug","category","created","updated"]
+        fields = ["id","user","title","body","slug","category","created","updated"]
